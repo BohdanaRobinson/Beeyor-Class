0591523f2893437b7876e9b3df23187947c7e24f
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-03-12 12:24:13 -0400

    New

---
+++ pages/BasePage.ts
@@ -0,0 +1,38 @@
+import { Page } from "@playwright/test";
+
+export default class BasePage {
+  page: Page;
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async waitUntilPageIsLoaded() {
+    await this.page.waitForEvent("domcontentloaded");
+  }
+
+  async navigate(url: string) {
+    await this.page.goto(url);
+  }
+
+  async click(selector: string) {
+    await this.page.locator(selector).click();
+  }
+
+  async fill(selector: string, value: string) {
+    await this.page.locator(selector).fill(value);
+  }
+
+  async getText(selector: string): Promise<string> {
+    const locator = this.page.locator(selector);
+    const text = (await locator.innerText()).trim();
+    return text;
+  }
+
+  async isVisible(selector: string): Promise<boolean> {
+    return this.page.locator(selector).isVisible();
+  }
+
+  async waitForElement(selector: string): Promise<void> {
+    await this.page.locator(selector).waitFor({ state: "visible" });
+  }
+}
